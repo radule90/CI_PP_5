f0671c07b567a98c76e4c97ba77fd5dd1bb815a3
radule90 <>
2023-07-29 23:37:50 +0000

    Add basic shop view, connect urls

--- product/views.py
+++ product/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def shop(request):
+    products = Product.objects.all().filter(is_available=True)
+
+    template = 'product/shop.html'
+    context = {
+        'products': products,
+    }
+    return render(request, template, context)
