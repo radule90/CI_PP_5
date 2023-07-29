ec40425b72d16a7bd0447d0124d9b5786906d67b
radule90 <>
2023-07-29 23:11:28 +0000

    Add Prodcut to admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}
+    list_display = ('is_available', 'product_name',
+                    'category', 'stock', 'price')
+    search_fields = ['product_name', 'description', 'price']
+    list_display_links = ('product_name', 'category',)
+
+
+admin.site.register(Product, ProductAdmin)
+
