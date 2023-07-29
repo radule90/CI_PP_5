2737f5f1863f59feaf5a8fb51bc4d28d1b0e34b9
radule90 <>
2023-07-29 21:52:52 +0000

    Add header with responsive style

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+// Header
+// Collapsible Navbar
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = (event) => {
+    event.preventDefault();
+    menu.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("fa-xmark");
+    navbar.classList.remove("active");
+};
+
+// Search Form Overlay
+document.querySelector("#search-icon").onclick = (event) => {
+    event.preventDefault();
+    document.querySelector("#search-form").classList.toggle("active");
+};
+document.querySelector("#close").onclick = (event) => {
+    event.preventDefault();
+    document.querySelector("#search-form").classList.remove("active");
+};
