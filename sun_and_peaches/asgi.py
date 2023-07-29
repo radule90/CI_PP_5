94a7fe973fb294c0f8e4e0d81a2912932f491392
radule90 <>
2023-07-29 21:03:27 +0000

    Initial commit

---
+++ sun_and_peaches/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sun_and_peaches project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sun_and_peaches.settings')
+
+application = get_asgi_application()
---
+++ sun_and_peaches/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for sun_and_peaches project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sun_and_peaches.settings')
+
+application = get_wsgi_application()
