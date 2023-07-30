05446b268e7fe1fb9bc92648b15703b6fca0450a
radule90 <>
2023-07-30 12:15:08 +0000

    Add variation manager, logic to display variations

--- product/models.py
+++ product/models.py
@@ -41,6 +41,25 @@ class Product(models.Model):
         return self.product_name
 
 
+class VariationManager(models.Manager):
+    '''
+    https://docs.djangoproject.com/en/4.2/topics/db/managers/
+    Manager model for variation
+    Filter variations based on category
+    '''
+    def sizes(self):
+        return super(VariationManager, self).filter(category='size',
+                                                    is_active=True)
+
+    def colors(self):
+        return super(VariationManager, self).filter(category='color',
+                                                    is_active=True)
+
+    def materials(self):
+        return super(VariationManager, self).filter(category='material',
+                                                    is_active=True)
+
+
 class Variation(models.Model):
     '''
     Model represents product variations
@@ -59,5 +78,7 @@ class Variation(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now=True)
 
+    objects = VariationManager()
+    
     def __str__(self):
         return self.value
