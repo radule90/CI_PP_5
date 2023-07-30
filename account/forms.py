452aa46d7fa23299a7043d682821826dd8b1934c
radule90 <>
2023-07-30 18:40:40 +0000

    Improve signup form style, add placeholders

--- account/forms.py
+++ account/forms.py
@@ -4,13 +4,31 @@ from phonenumber_field.formfields import PhoneNumberField
 
 
 class SignupForm(forms.ModelForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
-    phone_number = PhoneNumberField(help_text='Required')
+    email = forms.EmailField(max_length=200)
+    phone_number = PhoneNumberField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = Account
-        fields = ('email', 'password', 'first_name', 'last_name',
-                  'phone_number',)
+        fields = ('first_name', 'last_name', 'phone_number', 'email', 
+                'password', 'confirm_password')
 
     def __init__(self, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'password': 'Password',
+            'confirm_password': 'Confirm Password'
+        }
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
+
