eb4c1b135d233ad32826ee24e5a770c13efe125f
radule90 <>
2023-07-30 17:34:30 +0000

    Improve account admin panel, add comments

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    '''
+    Customizing Django Admin panel
+    '''
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'is_active')
+    filter_horizontal = ()
+    list_filter = ('is_admin', 'is_staff', 'is_active')
+    # Grouping fields
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff', 'is_active')}),
+    )
+    # Set as readonly
+    readonly_fields = ('last_login', 'date_joined')
+    # Order by date joined
+    ordering = ('-date_joined',)
+
+admin.site.register(Account, AccountAdmin)
