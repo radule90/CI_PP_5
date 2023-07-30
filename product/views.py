6263fb0905e095d334916a50689b763f38c45497
radule90 <>
2023-07-30 00:24:12 +0000

    Add product count logic for shop view

--- product/views.py
+++ product/views.py
@@ -7,18 +7,22 @@ def shop(request, category_slug=None):
     '''
     Function based view to list products in shop
     And filter products by category
+    Gives number of products in shop or category
     '''
     category = None
     products = None
     if category_slug != None:
         category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category, is_available=True)
+        product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
 
     template = 'product/shop.html'
     context = {
         'products': products,
         'category': category,
+        'product_count': product_count,
     }
     return render(request, template, context)
