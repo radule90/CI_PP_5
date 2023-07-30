363fe10e0810a4f396949e34a4b9384ce6460572
radule90 <>
2023-07-30 09:21:36 +0000

    Add add to cart functionality

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.cart, name='cart'),
+    path('add/<int:product_id>/', views.add, name='add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cart, CartItem
+from product.models import Product
 
 # Create your views here.
+
+def _cart_id(request):
+    '''
+    Function to get the current session cart ID.
+    '''
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
+
+
+def add(request, product_id):
+    '''
+    Function based view to add product in cart
+    Creates new cart if does not exist
+    Increments quantity
+    '''
+    product = Product.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(cart_id=_cart_id(request))
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            cart=cart,
+            quantity=1
+        )
+        cart_item.save()
+    print(cart_item)
+    return redirect('cart')
 
 
 def cart(request):
