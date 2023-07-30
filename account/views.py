d17f2bd5681dc2bc70558f9a398e10fe7c72cbd4
radule90 <>
2023-07-30 23:34:29 +0000

    Add part of logic for actiavation mail

--- account/views.py
+++ account/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import SignupForm
 from .models import Account
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 # Create your views here.
 
 
@@ -11,8 +17,22 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'You have successfully signed up!')
+            user = form.save()
+
+            # Activation Email
+            current_site = get_current_site(request)
+            mail_subject = 'Please Confirm Your E-mail Address'
+            message = render_to_string('account/signup_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            recipient = form.cleaned_data.get('email')
+            send_email = EmailMessage(mail_subject, message, to=[recipient])
+            print(send_email)
+            send_email.send()
+            messages.success(request, 'You have successfully signed up! Please confirm your email address to complete the registration.')
             return redirect('signup')
 
     else:
@@ -43,7 +63,7 @@ def signin(request):
     return render(request, template, context)
 
 
-@login_required(login_url = 'signin')
+@login_required(login_url='signin')
 def signout(request):
     if request.method == 'POST':
         logout(request)
@@ -51,3 +71,7 @@ def signout(request):
         return redirect('signin')
     template = 'account/signout_confirm.html'
     return render(request, template)
+
+
+def activate(request, uidb64, token):
+    return
--- sun_and_peaches/settings.py
+++ sun_and_peaches/settings.py
@@ -142,6 +142,14 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
