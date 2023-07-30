7c68260498d26569a0c816e970146812d845eb53
radule90 <>
2023-07-30 19:16:13 +0000

    Add logic for user sign up

--- account/forms.py
+++ account/forms.py
@@ -11,7 +11,7 @@ class SignupForm(forms.ModelForm):
 
     class Meta:
         model = Account
-        fields = ('first_name', 'last_name', 'phone_number', 'email', 
+        fields = ('first_name', 'last_name', 'phone_number', 'email',
                 'password', 'confirm_password')
 
     def __init__(self, *args, **kwargs):
@@ -32,3 +32,26 @@ class SignupForm(forms.ModelForm):
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].label = False
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            self.add_error('confirm_password', 'Password does not match!')
+        
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super(SignupForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['email'].split("@")[0]
+        user.password = self.cleaned_data['password']
+        user.set_password(self.cleaned_data['password'])
+        user.phone_number = self.cleaned_data['phone_number']
+
+        if commit:
+            user.save()
+        return user
--- account/views.py
+++ account/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from .forms import SignupForm
+from .models import Account
 # Create your views here.
 
+
 def signup(request):
-    form = SignupForm()
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = SignupForm()
     template = 'account/signup.html'
     context = {
         'form': form,
