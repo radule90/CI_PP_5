002416d354c48c267603f9e284da980a7d2005d8
radule90 <>
2023-07-30 11:59:01 +0000

    Add variation model to admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 
 # Register your models here.
 
@@ -14,5 +14,12 @@ class ProductAdmin(admin.ModelAdmin):
                    'is_available')
     list_editable = ('is_available',)
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'category', 'value', 'is_active')
+    list_editable = ('is_active',)
+    search_fields = ['category', 'value',]
+    list_filter = ('product', 'category', 'value', 'is_active')
+
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
