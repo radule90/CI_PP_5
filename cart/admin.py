63467315fb6e7c2a83592b32bf40bbbff47ec260
radule90 <>
2023-07-30 08:46:29 +0000

    Create cart models, add to admin panel

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
 # Register your models here.
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'created_at')
+
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity', 'is_active', 'created_at')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display_links = ('product',)
+    list_filter = ('cart', 'is_active', 'product',)
+    list_editable = ('is_active',)
+
+
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from product.models import Product
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    '''
+    Cart model for storing items added by users during their shopping session.
+    '''
+    cart_id = models.CharField(max_length=250, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+
+
+class CartItem(models.Model):
+    '''
+    CartItem model representing products added to a cart.
+    '''
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='cart_items')
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveIntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def subtotal(self):
+        '''
+        Method to calculate the subtotal for each CartItem.
+        '''
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return self.product.product_name
