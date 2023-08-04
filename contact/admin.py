31be180a90f95361ce9b554068d788629a71a6ff
radule90 <>
2023-08-04 21:13:29 +0000

    Add ContactAdmin and Contact to admin panel

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
+
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'subject', 'created_at']
+    search_fields = ['full_name', 'email', 'subject', 'message']
+    list_per_page = 10
+    
+admin.site.register(Contact, ContactAdmin)
