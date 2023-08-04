74363b45e6b15be7d6535b99fb7892e8e049ce47
radule90 <>
2023-08-04 20:55:07 +0000

    Add contact model and migrate

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    '''
+    Model represents contact form
+    '''
+    full_name = models.CharField(max_length=150, blank=False)
+    email = models.EmailField(max_length=150, blank=False)
+    subject = models.CharField(max_length=100, blank=False)
+    message = models.TextField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
