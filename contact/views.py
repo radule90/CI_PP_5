6bc0fcd1ace872646f91430d7805a442adb0d200
radule90 <>
2023-08-04 21:08:46 +0000

    Add contact form submti logic

--- contact/views.py
+++ contact/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Contact
+from django.contrib import messages
 # Create your views here.
 
 
 def contact(request):
+    '''
+    Function based view to handle contact message data from user
+    '''
+    if request.method == 'POST':
+        contact = Contact()
+        contact.full_name = request.POST.get('full_name')
+        contact.email = request.POST.get('email')
+        contact.subject = request.POST.get('subject')
+        contact.message = request.POST.get('message')
+        contact.save()
+        messages.success(request, 'Your message has been sent successfully.')
+        return redirect('contact')        
     template = 'contact/contact.html'
     context = {
         'active_contact': 'active_contact',
