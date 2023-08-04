346d517db5ed706d0e05117bb7a9a6050d8a98ab
radule90 <>
2023-08-04 22:21:20 +0000

    Add contactadmin filters, links

--- contact/admin.py
+++ contact/admin.py
@@ -6,6 +6,8 @@ from .models import Contact
 class ContactAdmin(admin.ModelAdmin):
     list_display = ['is_answered', 'full_name', 'email', 'subject', 'created_at']
     search_fields = ['full_name', 'email', 'subject', 'message']
+    list_display_links = ('is_answered', 'full_name', 'email',)
+    list_filter = ('is_answered',)
     list_per_page = 10
 
 admin.site.register(Contact, ContactAdmin)
