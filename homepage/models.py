61a3e815281cb20d14e3a4adc78ab43dc82b96b6
radule90 <>
2023-08-05 22:49:40 +0000

    Create Subscriber model, add to admin panel

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subscriber(models.Model):
+    full_name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
