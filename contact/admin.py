88d0de6b6ee68034f2be050bdeb40c59ed731adf
radule90 <>
2023-08-08 14:00:02 +0000

    PEP8 foramtting, comments for contact app

--- contact/admin.py
+++ contact/admin.py
@@ -4,10 +4,12 @@ from .models import Contact
 
 # Register your models here.
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ['is_answered', 'full_name', 'email', 'subject', 'created_at']
+    list_display = ['is_answered', 'full_name', 'email', 'subject',
+                    'created_at']
     search_fields = ['full_name', 'email', 'subject', 'message']
     list_display_links = ('is_answered', 'full_name', 'email',)
     list_filter = ('is_answered',)
     list_per_page = 10
+
 
 admin.site.register(Contact, ContactAdmin)
--- contact/views.py
+++ contact/views.py
@@ -9,14 +9,17 @@ def contact(request):
     Function based view to handle contact message data from user
     '''
     if request.method == 'POST':
+        # On post request create new instance
         contact = Contact()
+        # Fetch all data from post request
         contact.full_name = request.POST.get('full_name')
         contact.email = request.POST.get('email')
         contact.subject = request.POST.get('subject')
         contact.message = request.POST.get('message')
+        # Save instance, send successfull message, redirect bact to contact
         contact.save()
         messages.success(request, 'Your message has been sent successfully.')
-        return redirect('contact')        
+        return redirect('contact')
     template = 'contact/contact.html'
     context = {
         'active_contact': 'active_contact',
