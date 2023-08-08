4579286721085f28fb4e7934c13163903079c2cb
radule90 <>
2023-08-08 13:52:50 +0000

    PEP8 formatting category admin

--- category/admin.py
+++ category/admin.py
@@ -3,10 +3,12 @@ from .models import Category
 
 # Register your models here.
 
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('category_name',)}
     list_display = ('category_name', 'slug')
-    search_fields = ['category_name',]
-    list_display_links = ('category_name', 'slug',)
+    search_fields = ['category_name', ]
+    list_display_links = ('category_name', 'slug', )
+
 
 admin.site.register(Category, CategoryAdmin)
