06c1e0a1209a8ab61da1f47b168e9fa938e759b9
radule90 <>
2023-08-09 20:51:23 +0000

    Improve comments for homepage app

--- homepage/models.py
+++ homepage/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 class Subscriber(models.Model):
+    '''
+    Model representing a subscriber for the newsletter.
+    '''
     full_name = models.CharField(max_length=120)
     email = models.EmailField(max_length=100, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
